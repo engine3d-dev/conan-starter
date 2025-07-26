510c0088aeb03ac4f850ee0e2582126f511ce192
= <=>
2025-07-26 16:57:31 -0700

    Updated starter

--- application.cpp
+++ application.cpp
@@ -1,26 +1,42 @@
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 using namespace std;
 
 
 int main(){
-    //! @note Just added the some test code to test the conan-starter setup code
     if(!glfwInit()){
         fmt::print("glfwInit could not be initialized!\n");
         return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     
     int width = 800;
     int height = 600;
     std::string title = "Hello Window";
     GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    // Required to setup glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        return -1;
+    }
 
     while(!glfwWindowShouldClose(window)){
-        glfwPollEvents();
-        
         glfwSwapBuffers(window);
+        
+        glClearColor(0.5f, 1.0f, 1.0f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        glfwPollEvents();
     }
     return 0;
 }
