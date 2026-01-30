ebacf17b2b6fa0a387a1a76d6e3c1ef89ac61034
SpinnerX <heraaronhotmail@yahoo.com>
2026-01-29 22:05:03 -0800

    Cleaned up conan starter to being empty

--- conanfile.py
+++ conanfile.py
@@ -10,23 +10,27 @@ class StarterConanRecipe(ConanFile):
     name = "example"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
-    export_source = "CMakeLists.txt", "Application.cpp"
 
-    # Putting all of your build-related dependencies here
     def build_requirements(self):
-        self.tool_requires("make/4.4.1")
-        self.tool_requires("cmake/3.27.1")
+        self.tool_requires("cmake/[^4.0.0]")
+        self.tool_requires("ninja/[^1.3.0]")
+        self.test_requires("boost-ext-ut/2.3.1")
         self.tool_requires("engine3d-cmake-utils/4.0")
 
-    # Putting all of your packages here
+    # Putting the dependencies you use in the project here.
     def requirements(self):
-        self.requires("glfw/3.4", transitive_headers=True)
-        self.requires("spdlog/1.15.1")
-        self.requires("glm/1.0.1", transitive_headers=True)
-        self.requires("box2d/2.4.1", transitive_headers=True)
-        self.requires("opengl/system", transitive_headers=True)
-        self.requires("imguidocking/2.0")
+        pass
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
